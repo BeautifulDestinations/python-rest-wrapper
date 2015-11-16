1d464d58f227ef71a2e7d5076b9ecb967549cdb7
Hans <hans@hanshoglund.se>
2015-11-16 13:56:34 +0000

    Server working

--- rest-server.py
+++ rest-server.py
@@ -1,16 +1,58 @@
 #!flask/bin/python
 import six
 import time
+import threading
+import Queue
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.ext.httpauth import HTTPBasicAuth
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, static_url_path="")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 auth = HTTPBasicAuth()
+jobs = Queue.Queue()
 
-class Foo(object):
-    count = 22
+class Promise():
+    value = None
+    queue = Queue.Queue(1)
+    def fullfill(self, x):
+        try:
+            self.value = x
+            self.queue.put(None)
+        except Queue.Full:
+            return None
+    def sync(self):
+        while(True):
+            try:
+                self.queue.get(True, 1)
+                return self.value
+            except Queue.Empty:
+                x = 1
+                # Nothing
+        return None
+        # TODO
 
-foo = Foo()
+class Job:
+    filename   = None
+    parameters = None
+    promise   = None
+    def __init__(self, filename, parameters, promise):
+        self.filename   = filename
+        self.parameters = parameters
+        self.promise    = promise
+
+def job_handler():
+    while (True):
+        job = jobs.get()
+        print("Processing ", job.filename, job.parameters)
+        # Do image processing stuff with job.filename and job.parameters
+        # Send result to promise
+        job.promise.fullfill({'this':1, 'is':2, 'a':[1,2,3,4], 'result':True})
 
 @auth.get_password
 def get_password(username):
@@ -35,104 +77,30 @@ def bad_request(error):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+@app.route('/api/v1/predict', methods=['POST'])
+def uploadImage():
+    # return jsonify({'status': 'ok'})
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # TODO assure uploads directory
+        # Write to ./uploads
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # TODO post to job queue with future
 
+        prom = Promise()
+        jobs.put(Job(os.path.join(app.config['UPLOAD_FOLDER'], filename), {'myParameter':None}, prom))
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'],
-                                      _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-@auth.login_required
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-@auth.login_required
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': make_public_task(task[0])})
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-@auth.login_required
-def create_task():
-    if not request.json or 'title' not in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': make_public_task(task)}), 201
-
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-@auth.login_required
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and \
-            not isinstance(request.json['title'], six.string_types):
-        abort(400)
-    if 'description' in request.json and \
-            not isinstance(request.json['description'], six.string_types):
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description',
-                                              task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': make_public_task(task[0])})
-
-
-
-@app.route('/todo/api/v1.0/count', methods=['GET'])
-def count():
-    foo.count = foo.count + 1
-    # time.sleep(2)
-    return jsonify({'task': foo.count})
-
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-@auth.login_required
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+        r = prom.sync()
+        return jsonify({'status': r})
 
 
 if __name__ == '__main__':
+    t = threading.Thread(target = job_handler)
+    t.daemon = True
+    t.start()
     app.run(debug=True)
